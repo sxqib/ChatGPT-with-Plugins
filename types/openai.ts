036ceeba9fe439b5b8443af6c858b54fd7564512
Muhammad Saqib <m.sxqib@gmail.com>
2023-07-02 19:35:51 +0500

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -9,7 +9,7 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
+  GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
 }
@@ -24,11 +24,11 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.GPT_3_5_16K]: {
-    id: OpenAIModelID.GPT_3_5_16K,
-    name: 'GPT-3.5-16K',
+  [OpenAIModelID.GPT_3_5_AZ]: {
+    id: OpenAIModelID.GPT_3_5_AZ,
+    name: 'GPT-3.5',
     maxLength: 12000,
-    tokenLimit: 16000,
+    tokenLimit: 4000,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
