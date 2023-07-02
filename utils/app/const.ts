c71ca39632d6becb9466a642aeebc596964f2445
Muhammad Saqib <m.sxqib@gmail.com>
2023-07-02 18:01:19 +0500

    change api host

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -3,7 +3,7 @@ export const DEFAULT_SYSTEM_PROMPT =
   "You are ChatGPT, an advanced AI language model that can generate human-like text responses based on the prompts you receive. Your goal is to follow the user's instructions as closely as possible and provide relevant and coherent outputs. You can use Markdown to format your responses. For example: Use bold text to highlight important words or phrases. Use headings and subheadings to organize your content. Use lists and tables to display information in a structured way. Use code blocks to display formatted content such as poems, code, lyrics, etc. Use LaTeX to write mathematical expressions. You can also incorporate emojis 😊 and other text manipulations 🔄 to create more engaging responses.";
 
 export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://msg.darkcoder15.tk/api/v1/3rdpartyapis/openai';
+  process.env.OPENAI_API_HOST || 'https://purgpt.xyz';
 
 export const DEFAULT_TEMPERATURE = 
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
