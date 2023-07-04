7ca20a67adbf9f36b899c500564031e6551cfc3d
Muhammad Saqib <m.sxqib@gmail.com>
2023-07-04 16:36:10 +0500

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -416,7 +416,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                   </div>
 
                   {models.length > 0 && (
-                    <div className="flex h-full flex-col space-y-4 rounded-lg border border-neutral-200 p-4 dark:border-neutral-600">
+                    <div className="flex h-full flex-col space-y-2 rounded-lg border border-neutral-200 p-4 dark:border-neutral-600">
                       <ModelSelect />
                       <PluginSelect plugins={plugins} setPlugins={setPlugins} />
                       <SystemPrompt
