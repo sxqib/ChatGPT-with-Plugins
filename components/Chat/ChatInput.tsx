a0e06cede115e4783a6a04b40b880316b008705d
Muhammad Saqib <m.sxqib@gmail.com>
2023-07-04 14:14:28 +0500

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -340,12 +340,12 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://github.com/sxqib/ChatGPT-with-Plugins"
           target="_blank"
           rel="noreferrer"
           className="underline"
         >
-          ChatGPT
+          ChatGPT with Plugins
         </a>
         .{' '}
         {t(
