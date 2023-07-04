c01b9dc5c0c1691da94c5d6230f6b676865a21c3
Muhammad Saqib <m.sxqib@gmail.com>
2023-07-04 14:13:22 +0500

    Update home.tsx

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -360,8 +360,8 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
-        <meta name="description" content="ChatGPT but better." />
+        <title>ChatGPT with Plugins</title>
+        <meta name="description" content="Use the OpenAI API with plugins, in a similar UI like ChatGPT!" />
         <meta
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
